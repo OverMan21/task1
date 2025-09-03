14503049cd15bada1b717d9542b7610fe7825b9a
OverMan21 <vladimirrusanovschi@gmail.com>
2025-09-03 16:40:58 +0300

    eroare in proces

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,6 +5,7 @@ const Hero = () => {
         <div className="hero">
             <div className="flex-1 pt-36 padding-x">
                 <h1 className="hero_title">  </h1>
+                Find,book,or rent a car - quickly and easily!
         </div>
         </div>
     )
